37a96b2fa76df4c12701ba9f3921cb8786a0648f
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-21 18:29:27 -0300

    Criação do container principal

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,8 +1,20 @@
 import './conversor-moedas.css';
+import { Container, Row, Col, Button, Form, Spinner} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
   return (
-    <h1>Conversor Moedas</h1>
+    <Container>
+      <Row>
+          <h1>Conversor Moedas</h1>
+      </Row>
+      <Row>
+        <Col>
+        
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
